91995b55d31b15c191c7ce7c400fc4242b010b1e
Guadalupe <gabdelnour@frba.utn.edu.ar>
2024-10-05 02:34:59 -0300

    variables environment

--- src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
+++ src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.github.cdimascio.dotenv.Dotenv;
 import io.javalin.Javalin;
 
 import java.nio.channels.Channel;
@@ -18,8 +19,10 @@ import java.util.TimeZone;
 public class WebApp {
     public static void main(String[] args) {
 
-        var env = System.getenv();
-        var URL_VIANDAS = env.get("URL_VIANDAS");
+        Dotenv dotenv = Dotenv.load();
+        var URL_VIANDAS = dotenv.get("URL_VIANDAS");
+        var URL_HELADERAS = dotenv.get("URL_HELADERAS");
+        var URL_COLABORADORES = dotenv.get("URL_COLABORADORES");
 
         var objectMapper = createObjectMapper();
         var fachada = new Fachada();
@@ -27,7 +30,7 @@ public class WebApp {
         fachada.setViandasProxy(new ar.edu.utn.dds.k3003.clients.ViandasProxy(objectMapper));
         fachada.setHeladerasProxy(new ar.edu.utn.dds.k3003.clients.HeladerasProxy(objectMapper));
 
-        var port = Integer.parseInt(env.getOrDefault("PORT", "8080"));
+        var port = Integer.parseInt(dotenv.get("PORT"));
 
         var app = Javalin.create().start(port);
 
