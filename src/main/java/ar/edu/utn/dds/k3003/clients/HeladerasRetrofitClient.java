e60ee24f348b48aaf2e956a8626613a435ee27cb
Guadalupe <gabdelnour@frba.utn.edu.ar>
2024-10-06 23:09:24 -0300

    cambios conexiones h retirar

--- src/main/java/ar/edu/utn/dds/k3003/clients/HeladerasRetrofitClient.java
+++ src/main/java/ar/edu/utn/dds/k3003/clients/HeladerasRetrofitClient.java
@@ -2,13 +2,13 @@ package ar.edu.utn.dds.k3003.clients;
 
 import ar.edu.utn.dds.k3003.facades.dtos.RetiroDTO;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface HeladerasRetrofitClient {
     @POST("/retiros")
-    Call<Void> retirar(RetiroDTO retiro);
-
+    Call<Void> retirar(@Body RetiroDTO retiro);
     @POST("/depositos")
     Call<Void> depositar(Integer heladeraID, String qrVianda);
 
