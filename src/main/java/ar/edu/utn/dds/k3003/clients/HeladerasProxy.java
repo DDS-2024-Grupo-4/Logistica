7836c58eb7877d4bfaeb678bc3d0cc9820a46eda
Guadalupe <gabdelnour@frba.utn.edu.ar>
2024-11-02 01:19:56 -0300

    metodo depositar vianda

--- src/main/java/ar/edu/utn/dds/k3003/clients/HeladerasProxy.java
+++ src/main/java/ar/edu/utn/dds/k3003/clients/HeladerasProxy.java
@@ -59,7 +59,7 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 
         @Override
         public void depositar(Integer integer, String s) throws NoSuchElementException {
-           try {
+         /*  try {
                 Response<Void> response = service.depositar(integer,s).execute();
 
                 if (!response.isSuccessful()) {
@@ -72,6 +72,8 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
             } catch (IOException e) {
                 throw new RuntimeException("Error en la comunicación con el servicio de heladeras", e);
             }
+
+          */
 
 
         }
